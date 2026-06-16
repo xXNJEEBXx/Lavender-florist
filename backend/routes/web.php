3d92ba228e5603d6a9d9e868cba53ada42494201
NJEEB <xxnjeebxx@gmail.com>
2026-06-16 22:12:06 +0300

    fix: explicit storage route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/storage/{folder}/{filename}', function ($folder, $filename) {
+    $path = storage_path('app/public/' . $folder . '/' . $filename);
+    if (!file_exists($path)) {
+        abort(404);
+    }
+    
+    $mimeType = mime_content_type($path);
+    return response()->file($path, ['Content-Type' => $mimeType]);
+})->where('filename', '.*');
